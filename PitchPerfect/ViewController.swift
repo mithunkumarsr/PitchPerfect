08433ef634638939d241ae0069f47dceaa1c24c6
S R <rmithun@c4b301d5cbe7.ant.amazon.com>
2017-03-07 10:33:20 +0530

    First commit of pitch perfect app

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,16 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordingButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear called")
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(_ sender: AnyObject) {
+        print("record button was pressed")
+        recordingLabel.text = "Recording in Progress"
+    }
 
+    @IBAction func stopRecording(_ sender: AnyObject) {
+    }
 }
 
